960d3f4e90318fb6cb1d7ec8f1b7948ba4f218f7
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-04-09 22:34:25 +0530

    Add files via upload

---
+++ loop.prime number.py
@@ -0,0 +1,19 @@
+# i=1
+# while i<=100:
+#     c=0
+#     j=1
+#     while j<=i:
+#         if i%j==0:
+#             c=c+1
+#         j=j+1
+#     if c==2:
+#         print(i,"prime")        
+#     i=i+1    
+
+List = [[1,2],[3,2][5,6],[4],[89],[80]]
+sum=0
+i=0
+b=[]
+while i<len(list):
+    
+
---
+++ loop1.py
@@ -0,0 +1,1251 @@
+
+# a=234
+# print(a)
+###############################
+######variable
+# a="teena"
+# b=45
+# print(a+b)
+
+
+# _123="pooja"
+# a="karina"
+# print(_123+a)
+
+# _123=123
+# bhagwat="teena"
+# print(_123+bhagwat)
+
+# kulsa="teena"
+# bhagwat=55
+# print(kulsa,bhagwat)
+
+
+# muskan1="teena"
+# kul_sa="kanawar"
+# print(muskan1+kul_sa)
+
+
+# ########addtion and substraction in swapping
+# a=10
+# b=5
+# a=a*b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
+
+
+
+# Teena="k"
+# teena="v"
+# TEENA="P"
+# TEEna="l"
+# print(Teena+teena+TEENA+TEEna)
+
+
+
+# a=2
+# b=2
+# c=b
+# d=9
+# print(a,b,c,d)
+
+
+
+# a="my   name is    teenaranawat"
+# print("mynameisteena")
+# print("my         nameMissranawat")
+# print("teena")
+
+
+# a=10
+# b=6
+# c=9
+# a=a+c
+# b=c+b
+# c=b*a
+# print(a+b*c)
+
+# a=6
+# b=9
+# c=a*b
+# a=c+b
+# b=a*c
+# print(a*b+c)
+
+#data type################
+# a="teena"
+# print(type(a))
+
+
+# a=123
+# print(type(a))
+
+# a=5+7j
+# b=3
+# print(a+b)
+# print(type(a))
+
+# a="5"
+# b=6
+# c=2
+# a=int(a)
+# print(a+b+c)
+
+
+# b=( )
+# print(bool(b))
+
+# b="teena"
+# print(bool(b))
+
+
+# b="hello python"
+# print(bool(b))
+# print(bool(0))
+# print(bool())
+# print(bool(123))
+
+# a=True
+# b=False
+# print(a+b)
+
+
+
+#conversion###################
+
+# a="teena"
+# b=55
+# c=2.5
+# b=str(b)
+# c=str(c)
+# print(a+b+c)
+
+# a=34
+# b="27"
+# c=5
+# print(a,b)
+# b=int(b)
+# print(a+b)
+# b=str(b)
+# a=str(a)
+# print(a+b)
+# print(a*c+b)
+
+# a="Teena"
+# b=10
+# c="gytri"
+# a="kulsa"
+# print(a+c)
+# print(a+c)
+# print(a,b)
+
+
+# a=25
+# b="25"
+# c="radha"
+# d="25+3j"
+# a=str(a)
+# print(a+b+c)
+# print(a,b,c,d)
+# print(a*b*c)
+
+
+# a=2
+# b=34
+# print(a+b)
+
+
+# a=3%2
+# b=14/3
+# c=3**2
+# d=78//78
+# e=2*3
+# p=50-6
+# k=5+5
+# print(a,b,c,d,e,p,k,)
+
+
+
+# a=5
+# b=3
+# print(a+b)
+
+
+# a=57
+# v=37
+# a+=v
+# print(a)
+
+# a=3
+# b=5
+# a*=b
+# print(a)
+
+# a=5
+# b=a
+# print(a==b)
+# print(b!=a)
+
+# a=23
+# b=56
+# print(a<b)
+# print(a>b)
+
+
+# a=50
+# b=23
+# print(a>=b)
+# print(a<=b)
+
+
+
+
+# a=34
+# b=35
+# c=345
+# print(a<b and a>b)
+
+
+# x=22
+# print(x>10 and x<24)
+
+# q=12
+# b=11
+# c=67
+# print(q>b and c>q)
+
+
+#or operator
+
+# aa=45
+# hg=78
+# print(aa>hg or hg>aa)
+
+
+# t=25
+# k=56
+# c=45
+# print(t>k or c<t) 
+
+
+
+# a=True
+# b=False
+# print(a and b and a)
+# print(a and b or a)
+# print(not(a and b and a))
+
+# a=True
+# b=False
+# print(a or b or b)
+
+
+# b=45
+# c=34
+# print(not(b>c and c<b))
+
+
+
+# a="teena"
+# print("t"in a)
+# print("t"not in a)
+
+# n="teena"
+# print("pooja"in n )
+# print("pooja" not in n)
+
+
+
+# teena=45
+# kajal=33
+# print(teena is kajal)
+# print(teena is not kajal)
+
+
+# rina=22
+# pooja=rina
+# print(rina is pooja)
+# print(rina is not pooja)
+
+
+
+# a=23
+# k=44
+# print(a is not k)
+
+
+
+### if else ##############
+# if 4>5:
+#     print("5 is greater than 4")
+# else:
+#     print("chota hai")    
+
+
+# a=1000
+# b=4000
+# if a>=b:
+#     print("braber or equal")
+# else:
+#     print("baraber nhi hai")
+# 
+#     
+
+# a=44
+# b=200
+# c=a+b
+# if(c==123):
+#     print("equal")
+# else:
+#     print("not equal")    
+
+
+# day=input("enter the day")
+# if day=="monday":
+#     print("rajama chawal")
+# elif day=="tuesday":
+#     print("dal bati")
+# else:
+#     print("poha")        
+# day=input("enter the day")
+# if day=="monday":
+#     print("rajama chawal")
+# elif day=="tuesday":
+#     print("dal bati")
+# else:
+#     print("poha")        
+
+
+
+# age=int(input("enter the age"))
+# if age>=5 and age<=18:
+#     print("can go to school")
+# elif age>=21 and age<=26:
+#     print("can drive")   
+# elif age>=27 and age<=30:
+#     print("can merry")    
+# else:
+#     print("invalid age")    
+
+
+
+
+# varx=300-123
+# a=int(input("enrter the number"))
+# if varx==a:
+#     print(" equal")
+# else:
+#     print("not equal")  
+# 
+# w=int(input('enter the water'))  
+# if w<1:
+#     print("fill the water")
+# elif w<1 or w<10:
+#     print("no need water")
+# else:
+#     print("voer flow")        
+
+
+# a=12
+# b=5
+# if a>b:
+#     print("a is maximum")
+# else:
+#     print("b is less")    
+
+
+# n1=int(input('enter the n1'))
+# n2=int(input("enter the n2"))
+# n3=int(input("enter the n3"))
+# if n1>n2 or n1>n3 :
+#     print("n1 is maximum") 
+# elif n2>n3:
+#     print("n2 is maximum")
+# else:
+#     print("n3 is maximum")      
+
+# year=int(input("enter the year"))
+# if year%4==0:
+#     print("leap year") 
+# else:
+#     print("not leap year")        
+
+# a=int(input("enter the 1st numer"))
+# b=int(input("enter the the 2nd number"))
+# c=int(input("enter the 3rd number"))
+# if a>b>c or c>b>a:
+#     print(b,"is 2nd greater numbr")
+# elif a>c>b or b>c>a:
+#     print(c,"3rd greater number")
+# elif b>a>c or c>a>b:
+#     print(a,"1st greater number")     
+
+
+# m=int(input("enter the marks"))
+# if m>=1 and m<=45:
+#     print("grade F")
+# elif m>=25 and m<=45:
+#     print("grade E") 
+# elif m>=45 and m<=50:
+#     print("grade D")
+# else:
+#     print("B")         
+
+
+
+# age=int(input("enter the age"))
+# if age>=18:
+#     print(" can put vot")
+# else:
+#     print("not eligible age")  
+# 
+# 
+# num=int(input("enter the number"))
+# b=num%10
+# if b%3==0:
+#     print("divisible")
+# else:
+#     print("not divisible")
+# 
+# 
+# m=input("enter the month")
+# d=int(input("entwr the day"))
+# if m=="january"and (d<=31 and 1<=d):
+#     print("winter")
+# elif m=="febuary" and(d<=28 and 1<=d):
+#     print("wintr") 
+# elif m=="march" and (d>=1 and d<=31) :
+#     print("summer")     
+
+
+# cha=input("enter the alpha")
+# if cha>="a" and cha<="z":
+#     print("cha is alphabet")
+# else:
+#     print("not alpha bet")
+
+
+
+# m=(input("enter the month"))
+# if m=="january" or m=="march" or m=="may" or m=="july" or m=="augest" or m=="octember"or m=="decmbewr":
+#     print("31 day")
+# elif m=="aprial" or m=="jun" or m=="september" or m=="novomber":
+#     print("30 day")
+# else:
+#     print("28 day")  
+# 
+# 
+# a=int(input('enter the engle a'))
+# b=int(input("enter the engle b"))
+# c=int(input("enter the engle c"))
+# if a+b+c==180:
+#     print('triangle')
+# else:
+#     print("not triangle") 
+# 
+# 
+# a=int(input('enter the fist side'))
+# b=int(input('enter the second side'))  
+# c=int(input ('enter the third side'))
+# if a==b==c:
+#     print("equarilater engle")
+# elif a==b or b==c or c==a:
+#     print("isoscele")    
+# else:
+#     print("secolna")    
+
+
+# sell_p=int(input('enter the sell price'))
+# acutal_p=int(input('enter the acutal  price'))
+# if sell_p>acutal_p:
+#     print("profit")
+# else:
+#     print("loss")    
+
+
+# n=int(input('enter the mumber'))
+# a=n%10
+# b=(n//10)%10
+# # c=(n//100)%10
+# # d=(n//1000)%10
+# # print(a,b,c,d)
+# print(a,b)
+
+
+# alp=input('enter the alphabet')
+# if alp>="a"and alp<="z":
+#     print(alp)
+#     digit=int(input('enter the digit'))
+#     if digit>=999 and  digit<=9999:
+#         print(digit)
+#         sp=input("enter the specail charater")
+#         if sp=="@"or sp=="$"or sp=="&":
+#             print(sp)
+#             print(alp, digit, sp)
+#         else:
+#             print("worng")
+#     else:
+#         print("worng")
+# else:
+#     print("worng")                    
+
+
+
+# n=int(input('enter the number'))
+# if n>1 and n<10:
+#     print(n*1)
+#     print(n*2)
+#     print(n*3)
+#     print(n*4)
+#     print(n*5)
+#     print(n*6)
+#     print(n*7)
+#     print(n*8)
+#     print(n*9)
+#     print(n*10)
+
+
+
+# y=int(input('enter the year'))
+# if y>=1000 and y<=9999:
+#     print('yeAr')
+#     m=int(input('ente the month'))
+#     if m>=1 and m<=12:
+#         print("month")
+#         date=int(input('enter the day'))
+#         if date>=1 and date<=31:
+#             print('day')
+#             print(y," - ",m,' - ',date+1)
+
+
+# n=int(input('enter the number'))
+# n1=int(input('enter the number'))
+# if (n+n1)%3==0:
+#     print("odd")
+# else:
+#     print('odd nhi h')    
+
+
+# num=int(input('enter the number'))
+# if num%3==0 and num%7==0:
+#     print('navgurukul')
+# elif num%3==0:
+#      print('nav')
+# elif num%7==0:
+#     print("gurukul")
+           
+
+# c_y=int(input('enter the c_y'))
+# b_y=int(input('enter the b_y'))
+# if c_y-b_y:
+#     print("c_y",c_y-b_y)
+# else:
+#     print('worng age') 
+
+
+
+
+
+# num=int(input('enter the number'))
+# if num<0:
+#     print(-num)
+# else:
+#     print(-num)    
+
+# num=int(input('ente the number'))
+# a=num%10
+# b=num//10%10
+# c=num//100%10
+# print(a,b,c)
+
+
+# word=input("enter the word")
+# if "ing" in word:
+#     print(word+'ly')
+# else:
+#     print(word+"ing")    
+
+# i=10
+# while i>=0:
+#   print(10-i)
+#   i=i-1
+
+
+# a=int(input("enter the number"))
+# b=len(str(a))
+# c=b-3
+# d=(a//10**c)%10
+# print(d)
+
+
+
+
+# y=int(input("enter the year"))
+# if y%100==0:
+#     if y%400==0:
+#         print("leap year")
+#     else:
+#         print('not leap year') 
+# else:
+#     if y%4==0:
+#         print("leap year")
+#     else:
+#         print("not leap year")               
+
+
+#######################################################################
+#LOOP
+
+
+
+# i=1
+# while i<=10:
+#     print(i)
+#     i=i+1
+
+# t=int(input('enter the number'))
+# k=int(input('enter the number'))
+# i=1
+# s=0
+# while i<=k:
+#     s=s+t
+#     print(s,"*",i)
+#     i=i+1
+
+
+# i=2
+# while i<=10:
+#     print(i)
+#     i=i+2
+
+
+# i=1
+# while i<=100:
+#     if i%2==0:
+#          print(-i,end="  ")
+#     else:
+#         print(i,end="  ")
+#     i=i+1        
+
+
+# sum=0
+# i=1
+# while i<=100:
+#     sum=sum+i
+#     print(sum)
+#     i=i+1      
+
+
+
+# i=440
+# while i<460:
+#     c=i-439
+#     print(c)
+#     i=i+1
+
+# i=0
+# while i<=50:
+#     # if i!=0:
+#     print(i)
+#     i=i+6
+
+
+#break
+
+# i=1
+# while i<=10:
+#     i=i+1
+#     print(i)
+#     if i==7:
+#         break
+
+# i=1 
+# while i<10:
+#     i=i+1
+#     if i==5 or i==9 or i==2:
+#         continue
+#     print(i)
+
+# i=1
+# s=0
+# while i<=10:
+#     n=int(input("enter the number"))
+#     s=s+n
+#     # print(s)
+#     i=i+1
+#     print(s)
+
+# i=156
+# while i<=246:
+#     z=i-155
+#     print(z)
+#     i=i+1
+
+#prime number
+# n=int(input('enter the number'))
+# c=0
+# i=1
+# while i<=n:
+#     if n%i==0:
+#         c=c+1
+#     i=i+1
+# if c==2:
+#     print("prime number")
+# else:
+#     print("not prime number")   
+# 
+# 
+# s=0
+# a=0
+# i=1
+# while i<11:
+#     n=int(input("enter the weghit"))
+#     s=s+n
+#     a=s/11
+#     i=i+1
+# print(s)
+# if a%5==0:
+#     print("divisible")
+# else:
+#     print("not divisible")    
+# 
+# 
+# n=int(input("enter the number"))
+# i=n
+# s=0
+# while 0<i:
+#     r=i%10
+#     s=s+r**3
+#     i=i//10
+# if s==n:
+#     print("armstormg") 
+# else:
+#     print("not arm")            
+
+
+
+
+
+# n=int(input('enter the number'))
+# f=1
+# while 0<n:
+#     f=f*n
+#     n=n-1
+# print("factorial=",f)
+
+
+
+
+# n=int(input("enter the number"))
+# s=0
+# i=1
+# while i<n:
+#     if n%i==0:
+#         s=s+i
+#     i=i+1
+# if(s==n):
+#     print("perfect")
+# else:
+#     print("not perfact")             
+
+
+# n=int(input('enter the number'))
+# s=0
+# i=0
+# while 0<n:
+#     r=n%10
+#     i=i*10+r
+#     n=n//10
+#     print("reverse",i)
+
+
+# n=int(input("enter the number"))
+# i=1
+# while i<=10:
+#     print(n,"*",i,"=",i*n)
+#     i=i+1
+
+
+
+# n=int(input("enter the number"))
+# a=0
+# b=1
+# c=0
+# i=1
+# while i<=n:
+#     print(c,end="  ")
+#     a=b
+#     b=c
+#     c=a+b
+#     i=i+1
+
+# i=100
+# while i<=500:
+#     if i%13==0 and i%3!=0:
+#         print(i,end='  ')
+#     i=i+1    
+
+# n=int(input('enter the number'))
+# st="2"
+# i=0
+# while i<=n:
+#     st=st+"2"
+#     print(st,end='  ')
+#     i=i+1
+
+# i=10
+# while i>=1:
+#     print(i,11-i,)
+#     i=i-1
+
+# i=1
+# j=49
+# while i<=j and j>=i:
+#     print(i,"----",j)
+#     i=i+1
+#     j=j-1
+
+
+# c=1
+# i=1
+# while i<=3:
+#     j=1
+#     while j,
+
+
+# n=int(input("enter the number"))
+# i=1
+# while i<n:
+#     f=0
+#     j=1
+#     while j<=i:
+#         if i%j==0:
+#             f=f+1
+#         j=j+1
+#     if f==2:
+#         print(i,"prim number") 
+#     else:
+#         print(i,"not prime") 
+#     i=i+1          
+
+# i=1                         #1
+# while i<=5:                 #12
+#     j=1                     #123
+#     while j<=i:             #1234
+#         print(j,end='  ')   #12345
+#         j=j+1
+#     print()
+#     i=i+1    
+
+# A
+# AB
+# ABC
+# ABCD
+
+
+
+# i=65
+# while i<=69:
+#     j=65
+#     while j<=i:
+#         print(chr(j),end="  ")
+#         j=j+1
+#     print( ) 
+#     i=i+1    
+
+# 54321
+# 5432
+# 543
+# 54
+# 5
+
+# i=1
+# while i<=5:
+#     j=5
+#     while j>=i:
+#         print(j,end='  ')
+#         j=j-1
+#     print( )
+#     i=i+1  
+# 
+# 
+# # A
+# AB
+# ABC
+# ABCD
+
+# A
+# AB
+# ABC
+# ABCD
+
+# 55555
+# 4444
+# 333
+# 22
+# 1  
+# n=int(input("enter the number"))
+# i=1
+# while 0<n:
+#     j=1
+#     while j<=n:
+#         print(n,end=" ")
+#         j=j+1
+#     print()
+#     i=i+1 
+#     n=n-1   
+
+#     1
+#    12
+#   123
+#  1234
+# 12345
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5-i:
+#         print(" ",end="")
+#         j=j+1
+#     b=1
+#     while b<=i:
+#         print(b,end='')
+#         b=b+1
+#     print( )
+#     i=i+1      
+# 
+# i=1
+# while i<=5:
+#     b=1
+#     while b<=5-i:
+#         print("-",end=" ")
+#         b=b+1
+#     j=1    
+#     while j>=i:
+#         print(i,end=" ")
+#         j=j+1
+#     print()
+#     i=i-1    
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print("*",end="  ")
+#         j=j+1
+#     print()
+#     i=i+1    
+# A
+# AB
+# ABC
+# ABCD
+
+
+#         *
+#       * *
+#     * * *
+#   * * * * 
+# * * * * *    
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5-i:
+#         print("-",end=" ")
+#         j=j+1
+#     b=1
+#     while b<=i:
+#         print("*",end=" ") 
+#         b=b+1   
+#     print( )
+#     i=i+1 
+#        
+#        *
+#      * * *
+#    * * * * * 
+#   * * * * * * *
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5-i:
+#         print(" ",end=" ")
+#         j=j+1
+#     b=1
+#     while b<=i:
+#         print("*",end=" ") 
+#         print(" ",end=" ")   
+#         b=b+1
+#     print()
+#     i=i+1    
+
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5:
+#         print(j,end="  ")
+#         j=j+1
+#     print() 
+#     i=i+1   
+
+
+
+
+# a
+# ab
+# abc
+# abcd
+# abcde
+# i=97
+# while i<=101:
+#     j=97
+#     while j<=i:
+#         print(chr(j),end=" ")
+#         j=j+1
+#     print() 
+#     i=i+1 
+# 
+#   
+# 1
+# 4  4
+# 9  9   9
+# 16 16 16 16
+# 25 25 25 25 25
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print(i**2,end=" ")
+#         j=j+1
+#     print()    
+#     i=i+1
+
+# i=1
+# a=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print(a,end=" ")
+#         j=j+1
+#     print() 
+#     i=i+1
+#     a=a+2
+# 
+# 1
+# 2 3 4
+# 5 6 7 8 9
+  
+# i=1
+# k=1
+# while i<=6:
+#     j=1
+#     while j<=i:
+#         print(k,end="  ") 
+#         j=j+1
+#         k=k+1    
+#     print( )
+#     i=i+2
+# 0
+# 0 1
+# 0 2 4
+# 0 3 6 9
+# 0 4 8 12 16
+
+# i=0
+# while i<=4:
+#     j=0
+#     while j<=i:
+#         print(j*i,end=" ")
+#         j=j+1
+#     print()
+#     i=i+1
+
+
+#    1
+#   222
+#  3333333    
+#  4444444
+# 555555555
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=5-i:
+#         print(" ",end=" ")
+#         j=j+1
+#     b=1
+#     while b<=i:
+#         print(i,end=" ")
+#         print(" ",end=" ")  
+#         b=b+1
+#     print( )
+#     i=i+1 
+# 
+#      
+# 12345
+#  1234
+#   123
+#    12
+#     1
+# i=5
+# while i>=1:
+#     j=1
+#     while j<=5-i:
+#         print("-",end=" ")
+#         j=j+1
+#     b=1
+#     while b<=i:
+#         print(b,end=" ")
+#         b=b+1
+#     print( )
+#     i=i-1 
+# 
+# 
+
+
+
+# p
+# py
+# pyh
+# pyho
+# pyhon
+         
+# a="python"
+# i=0
+# while i<len(a):
+#     j=0
+#     while j<=i:
+#         print(a[j],end=" ")
+#         j=j+1
+#     print( )
+#     i=i+1    
+
+
+
+# a="bhagwtteena"
+# i=0
+# while i<len(a):
+#     j=0
+#     while j<=i:
+#         print(a[j],end="")
+#         j=j+1
+#     print( )
+#     i=i+1
+
+#   *
+#  ***   
+# *****
+#  ***  
+#   *
+
+# c=1
+# i=1
+# while i<=3:
+#     j=1
+#     while j<=3-i:
+#         print(" ",end=" ")
+#         j=j+1
+#     k=1
+#     while k<=c:
+# #         print("*",end="")
+#         print(" ",end="")
+#         k=k+1
+#     print()
+#     c=c+2
+#     i=i+1 
+# l=4
+# c=3
+# while l<=6:
+#     j=1
+#     while j<=l-3:
+#         print(" ",end=" ")
+#         j=j+1
+#     m=1
+#     while m<=c:
+#         print("*",end=" ")
+#         print("",end=" ")
+#         m=m+1
+#     print( )
+#     c=c-2
+#     l=l+1 
+# 
+#    
+# 1 1 1 1 1 1
+# 2 2 2 2 2 2
+# 3 3 3 3 3 3 
+# 4 4 4 4 4 4
+# 5 5 5 5 5 5
+
+# i=1
+# while i<=5:
+#     j=5
+#     while j>=1:
+#         print(i,end=" ")
+#         j=j-1
+#     print() 
+#     i=i+1   
+
+
+# v=3.12
+# l=3
+# print(v+l)
+
+
+
+# k="pooja"
+# print(type(k))
+
+
+# k="teena"
+# p=34
+# p=str(p)     
+# print(k,p)
+
+# k=4+7j
+# p="teena"
+# p=34
+# p=str(p)
+# print(k,p)
+
+
+# k=4+7j
+# p="teena"
+# k=str(k)
+
+# print(k+p)k=4+7j
+# p="teena"
+
+# l=6
+# b=2
+# # l+=b
+# # l-=b
+# l*=b
+
+# print(l)
+
+
+# a=True
+# b=False
+# print(a and b or a or not b)
+
+
+# num=int(input("enter the number"))
+# a=num%10
+# b=num//100%10
+# c=num//1000%10
+# d=num//10000%10
+# print(a,b,c,d)
+
+# # # print(a,b)
+# # print(a)
+
